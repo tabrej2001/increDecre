7e8513918b59163a58beef3d988141dfd2ee6bcc
Tabrej Alam <tabrejalam76224@gmail.com>
2022-06-24 16:07:30 +0530

    first commit

---
+++ src/test1.jsx
@@ -0,0 +1,19 @@
+
+
+function Welcome(){
+    return (
+    <div>
+    
+     <h1>Welcome To React Class</h1>
+
+
+    </div>)
+}
+//  function Thanksmsg(){
+//     <div>
+
+//         <h1>Thanks Everybody</h1>
+//     </div>
+// }
+
+export default Welcome;
